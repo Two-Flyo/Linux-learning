57dbf8069f014c969ab31a4f2083eae5cfe67575
lrf <465197183@qq.com>
2022-05-14 23:28:50 +0800

    IO相关

--- IO/l3/redir.c
+++ IO/l3/redir.c
@@ -4,15 +4,20 @@
 
 int main()
 {
+	//系统接口
 	const char* msg1 = "hello stdin!\n";
 	write(1,msg1,strlen(msg1));
 
-	const char* msg2 = "hello stdout!\n";
-	write(2,msg2,strlen(msg2));
+	//const char* msg2 = "hello stdout!\n";
+	//write(2,msg2,strlen(msg2));
 	
-	printf("hello printf\n");
-	fprintf(stdout,"hello fprintf\n");
-	
-	close(1);
+	//C语言接口
+	printf("hello printf\n");	       //stdout 
+	fprintf(stdout,"hello fprintf\n"); //stdout 
+	fputs("hello fputs\n",stdout);       //stdout 
+	fflush(stdout);
+
+	//close(1);
+	fork();
 	return 0;
 }
